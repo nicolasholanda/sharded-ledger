a7dc4e4208b89358e41ebaa5fde000f1cf261d6b
nicolasholanda <nikolas.nhm14@gmail.com>
2026-07-16 10:11:36 -0300

    Initial commit

---
+++ src/main/java/com/github/nicolasholanda/shardedledger/ShardedLedgerApplication.java
@@ -0,0 +1,13 @@
+package com.github.nicolasholanda.shardedledger;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ShardedLedgerApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ShardedLedgerApplication.class, args);
+    }
+
+}
